6ebe699cda08fbc66f213106f9b288d5d6599e17
Rahul Parmar <rahulparmar636790@gmail.com>
2024-10-13 18:17:35 +0530

    Passing a Variable in React Component(funtion)

--- 01-vitereact/src/App.jsx
+++ 01-vitereact/src/App.jsx
@@ -2,11 +2,13 @@ import Cup from "./chai"
 
 function App() {
 
+  const userName = "rahulparmar"
+
   return (
     <>
     <Cup/>
-    <h1>Hello</h1>
-    <p>many elements returned...</p>
+    <h1>Hello {userName}</h1>
+    <p>Test paragraph</p>
     </>
   )
 }
@@ -15,3 +17,8 @@ export default App
 
 // sins you cant return more than one element in react
 //  to return more elements , just wrap them in a div or blank tag <> </>
+//
+
+/* to return/output variables in react :- u can use them in {} ...
+   only Evaluated Expression should be writen in {} 
+   means - Final outcome only like the variable...you can not write any condition or loop in it. */
