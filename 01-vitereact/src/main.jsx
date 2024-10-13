e1bca3544c77960c5e57a118e59f9fc46e2944f4
Rahul Parmar <rahulparmar636790@gmail.com>
2024-10-13 18:18:41 +0530

    learning React side conversions/Syntax of  components

--- 01-vitereact/src/main.jsx
+++ 01-vitereact/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return  (
+    <div>
+      <h1>Custom APP!</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
+
+// an Object with an 'a' tag of html that is what react supposed to get...and convert it to their syntax term to execute....
+const AnotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+// created a variable - anotherUser
+const anotherUser = "Rahul"
+
+// React side Syntax of how react converts our functions or components before rendering on Page.
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target:'_blank'},
+  'Click me to Visit Google ',
+  anotherUser
+)
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  reactElement
 )
