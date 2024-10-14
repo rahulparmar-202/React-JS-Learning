9d582c24a61ab1c7d5bf0d6f13f97b4512e3f657
Rahul Parmar <rahulparmar636790@gmail.com>
2024-10-14 17:41:07 +0530

    Assignment - 1 : value not less than 0 and greater than 20

--- 02-Counter/src/App.jsx
+++ 02-Counter/src/App.jsx
@@ -1,31 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
+  //Hooks
   // counter and setCounter are just names , we can change them to our own also.
-  let [counter, setCounter] = useState(0)
+  let [counter, setCounter] = useState(0);
 
   // let counter = 5
 
-  // to add value 
-  const addValue = ()=>{
-  /* //1 way
-   counter = counter + 1
-   setCounter(counter)
-  */
-    
-   // 2nd Way
-   setCounter(counter + 1)
-   
-  }
+  // to add value
+  const addValue = () => {
+    if (counter >= 20) {
+      // when counter is either 20 or more than 20 , then will not add more.
+      setCounter(counter);
+    } else {
+      // 2nd Way
+      setCounter(counter + 1);
+    }
+
+    /* //1 way
+     counter = counter + 1
+     setCounter(counter)
+    */
+  };
 
   // to remove value
-  const removeValue = () =>{
-    setCounter(counter-1)
-  }
+  const removeValue = () => {
+    if (counter <= 0) {
+      // when counter is either 0 or less than 0 , then will not remove values
+      setCounter(counter);
+    } else {
+      setCounter(counter - 1);
+    }
+  };
 
   return (
     <>
@@ -35,7 +44,11 @@ function App() {
       <br />
       <button onClick={removeValue}>Remove Value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// 1 - Assignement :-
+//  a. value should not go in negative
+//  b. addValue should not be more than 20
